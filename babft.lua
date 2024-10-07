3e75de16e1dbe54ac5bcca10079b5a6789f248c7
21ZeroDay12 <122727433+21ZeroDay12@users.noreply.github.com>
2024-10-07 22:00:09 +0700

    babft.lua

---
+++ babft.lua
@@ -0,0 +1,37 @@
+local a = workspace.BoatStages.NormalStages.CaveStage1.DarknessPart.CFrame
+local b = workspace.BoatStages.NormalStages.CaveStage2.DarknessPart.CFrame
+local c = workspace.BoatStages.NormalStages.CaveStage3.DarknessPart.CFrame
+local d = workspace.BoatStages.NormalStages.CaveStage4.DarknessPart.CFrame
+local e = workspace.BoatStages.NormalStages.CaveStage5.DarknessPart.CFrame
+local f = workspace.BoatStages.NormalStages.CaveStage6.DarknessPart.CFrame
+local g = workspace.BoatStages.NormalStages.CaveStage7.DarknessPart.CFrame
+local h = workspace.BoatStages.NormalStages.CaveStage8.DarknessPart.CFrame
+local i = workspace.BoatStages.NormalStages.CaveStage9.DarknessPart.CFrame
+local j = workspace.BoatStages.NormalStages.CaveStage10.DarknessPart.CFrame
+local end_ = workspace.BoatStages.NormalStages.TheEnd.GoldenChest.Trigger.CFrame
+local yapidor = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+local waiting = 1.89
+game.Workspace.Gravity = 0
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = a
+wait(waiting)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = b
+wait(waiting)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = c
+wait(waiting)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = d
+wait(waiting)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = e
+wait(waiting)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = f
+wait(waiting)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = g
+wait(waiting)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = h
+wait(waiting)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = i
+wait(waiting)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = j
+wait(waiting)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = end_
+wait(14)
+game.Workspace.Gravity = 100
